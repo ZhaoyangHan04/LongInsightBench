8663d42030b99f6e153b32d738d40c4d76bb0a30
tu-naaa <148608829+tu-naaa@users.noreply.github.com>
2025-10-06 23:02:52 +0800

    Change model and file paths to relative paths
    Updated file paths to use relative paths instead of absolute paths.

--- src/test/VideoLLaMA3/main.py
+++ src/test/VideoLLaMA3/main.py
@@ -9,7 +9,7 @@ import numpy as np
 import subprocess
 
 # ====== 初始化模型 ======
-MODEL_PATH = "/data1/lianghao/models/VideoLLaMA3-7B"
+MODEL_PATH = "./models/VideoLLaMA3-7B"
 model = AutoModelForCausalLM.from_pretrained(
     MODEL_PATH,
     trust_remote_code=True,
@@ -23,9 +23,9 @@ USE_AUDIO_IN_VIDEO = True
 # ====== 文件路径 ======
 current_tasks = ["1intra_event_reasoning", "3audio_visual_alignment", "5topic_stance_evolution_summarization", "4timeline_reconstruction", "6cross_event_causality", "2multimodal_temporal_localization"]
 for current_task in current_tasks:
-    INPUT_FILE = f"/data1/lianghao/hzy/lqh/final_qa_subset/{current_task}.json"
-    OUTPUT_FILE = f"/data1/lianghao/hzy/lqh/experiment_frames/videollama_7b/32/{current_task}.json"
-    VIDEO_ROOT = "/data1/lianghao/hzy/lqh/datasets/finevideo/videos"
+    INPUT_FILE = f"./final_qa_subset/{current_task}.json"
+    OUTPUT_FILE = f"./experiment_frames/videollama_7b/32/{current_task}.json"
+    VIDEO_ROOT = "./datasets/finevideo/videos"
     os.makedirs(os.path.dirname(OUTPUT_FILE), exist_ok=True)
 
     # ====== 读取已有结果 ======
